ce7b0f23d99f6cd9c2b287eb01007871d505d37b
r-higaki <r-higaki@outlook.jp>
2020-06-09 23:48:22 +0900

    Updates

---
+++ precache-manifest.f1160a2f4d76ca0c3683260eda70f633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37047b9d24e042b7bf6edce4ba7955e5",
+    "url": "/reactbooklist/index.html"
+  },
+  {
+    "revision": "dd1cf8296259e748658a",
+    "url": "/reactbooklist/static/css/main.1ecde969.chunk.css"
+  },
+  {
+    "revision": "d016ea25dadd18da084a",
+    "url": "/reactbooklist/static/js/2.35bef39b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactbooklist/static/js/2.35bef39b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1cf8296259e748658a",
+    "url": "/reactbooklist/static/js/main.cd19bc52.chunk.js"
+  },
+  {
+    "revision": "a03432524f74c6883799",
+    "url": "/reactbooklist/static/js/runtime-main.1d1b5cab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f0bd8ee43ef228e7bad57c8c5b719208.js"
+  "/reactbooklist/precache-manifest.f1160a2f4d76ca0c3683260eda70f633.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactbooklist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
